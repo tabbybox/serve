04887fe76591c337c0fb2abe53e1acc14b6281a4
Mio Nekoya <admin@mio9.sh>
2024-02-25 06:36:05 +0900

    update build script

--- build-all.sh
+++ build-all.sh
@@ -1,14 +1,17 @@
+#!/bin/bash
 for os in darwin linux windows
 do
     for arch in 386 amd64 arm64
     do  
-        if [ $os = "darwin"] && [ $arch = "386" ]
+        if [[ $os -eq "darwin" && $arch -eq "386" ]]
         then
             echo Skipping Apple-x86
             continue
         fi
         echo Building for $os-$arch
-        GOOS=$os GOARCH=$arch go build -o build/serve-$os-$arch
+        GOOS=$os GOARCH=$arch go build -o build/serve-$os-$arch &
         # echo $os-$arch
     done
 done
+wait
+echo "Compile complete"
